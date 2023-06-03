db37f8a751b9897d7bdfe7e9649a681dc342fae7
Simon De Boe <simondeboe7090@gmail.com>
2023-06-03 16:08:30 +0200

    Instalation du projet-EnityPost-Slugauto-uplaoderdefichier-fixtures-fakerphp

---
+++ src/DataFixtures/PostFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Post\Post;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class PostFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 150; $i++) {
+            $post = new Post();
+            $post->setTitle($faker->sentence(mt_rand(3, 6)))
+                ->setContent($faker->realText(1800))
+                ->setState(mt_rand(0, 2) === 1 ? Post::STATES[0] : Post::STATES[1]);
+
+            $manager->persist($post);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Post/Post.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace App\Entity\Post;
+
+use Cocur\Slugify\Slugify;
+use App\Repository\Post\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+#[UniqueEntity(
+    'slug', message: 'Ce slug existe déjà.')]
+class Post
+{
+
+    const STATES = ['STATE_DRAFT','STATE_PUBLISHED','STATE_ARCHIVED'];
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Assert\NotBlank]
+    #[ORM\Column(type: 'string', length: 255,unique: true)]
+    private string $title;
+
+    #[Assert\NotBlank]
+    #[ORM\Column(type: 'string',length: 255,unique: true)]
+    private string $slug;
+
+    #[Assert\NotBlank]
+    #[ORM\Column(type: 'text')]
+    private string $content;
+
+    #[Assert\NotBlank]
+    #[ORM\Column(type: 'string',length: 255)]
+    private string $state = Post::STATES[0];
+
+    #[Assert\NotNull]
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $createdAt;
+
+    #[Assert\NotNull]
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $updatedAt;
+
+    #[ORM\OneToOne(inversedBy:'post',targetEntity: Thumbnail::class, cascade: ['persist', 'remove'])]
+    private Thumbnail $thumbnail;
+
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+        $this->updatedAt = new \DateTimeImmutable();
+
+    }
+
+    #[ORM\PrePersist]
+    public function prePersist()
+    {
+        $this->slug = (new Slugify())->slugify($this->title);
+    }
+    #[ORM\PreUpdate]
+    public function preUpdate(): self
+    {
+        $this->updatedAt = new \DateTimeImmutable();
+    }
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param string $slug
+     */
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param string $state
+     */
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTimeImmutable $createdAt
+     */
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getUpdatedAt(): \DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTimeImmutable $updatedAt
+     */
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return Thumbnail
+     */
+    public function getThumbnail(): Thumbnail
+    {
+        return $this->thumbnail;
+    }
+
+    /**
+     * @param Thumbnail $thumbnail
+     */
+    public function setThumbnail(Thumbnail $thumbnail): self
+    {
+        $this->thumbnail = $thumbnail;
+        return $this;
+    }
+}
