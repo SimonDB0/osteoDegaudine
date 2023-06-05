ad3ab2dd44b3311388296d62b56d1e1e63ba545c
Simon De Boe <simondeboe7090@gmail.com>
2023-06-05 21:17:19 +0200

    mise en place du header

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,23 @@
+
 /*
  * Welcome to your app's main JavaScript file!
  *
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import 'tw-elements'
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Webpack Encore is working !');
+// menu hamburger
+document.addEventListener('DOMContentLoaded', (event) => {
+    const navButton = document.querySelector('[data-te-collapse-init]');
+    const navMenu = document.querySelector('#navbarSupportedContentY');
 
-})
+    navButton.addEventListener('click', () => {
+        navMenu.classList.toggle('hidden');
+        navMenu.classList.toggle('flex');
+    });
+});
+
