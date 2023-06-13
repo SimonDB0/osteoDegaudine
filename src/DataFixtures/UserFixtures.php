231e5f62a4b8049aa1c983fb20810d8bcfd63045
Simon De Boe <simondeboe7090@gmail.com>
2023-06-13 15:36:09 +0200

    mise en place de l'entity user

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+    }
+
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        $user = new User();
+        $user->setEmail('admin@admin.be')
+            ->setLastname('De Boe')
+            ->setFirstname('Simon')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword(
+                $this->hasher->hashPassword(
+                    $user,
+                    'password'
+                )
+
+            );
+        $manager->persist($user);
+
+        for ($i = 0; $i < 15; $i++) {
+            $user = new User();
+            $user->setEmail($faker->email())
+                ->setLastname($faker->lastname())
+                ->setFirstname($faker->firstname())
+                ->setPassword(
+                    $this->hasher->hashPassword(
+                        $user,
+                        'password'
+                    )
+                );
+
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
